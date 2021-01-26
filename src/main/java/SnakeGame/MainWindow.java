20b2f2d8900f6b50dc857cde63c1d8c7e2fac7a3
GGr3ml1N <dinars163@gmail.com>
2021-01-26 15:25:00 +0300

    Создан конструктор MainWindow отвечающий за основные параметры игрового окна

--- src/main/java/SnakeGame/MainWindow.java
+++ src/main/java/SnakeGame/MainWindow.java
@@ -1,4 +1,15 @@
 package SnakeGame;
 
-public class MainWindow {
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow() {
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320,345);
+        setLocation(400,400);
+        setVisible(true);
+    }
+
 }
