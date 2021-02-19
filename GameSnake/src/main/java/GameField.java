6c0106a388afd95ff79e65c9ea53f96fb92d4a68
GGr3ml1N <dinars163@gmail.com>
2021-02-20 01:08:51 +0300

    Прописан метод checkCollisions

--- GameSnake/src/main/java/GameField.java
+++ GameSnake/src/main/java/GameField.java
@@ -77,6 +77,24 @@ public class GameField extends JPanel implements ActionListener {
     }
 
     private void checkCollisions() {
+        for (int i = dots; i >0 ; i--) {
+            if(i>4 && x[0] == x[i] && y[0] == y[i]){
+                inGame = false;
+            }
+        }
+
+        if(x[0]>SIZE){
+            inGame = false;
+        }
+        if(x[0]<0){
+            inGame = false;
+        }
+        if(y[0]>SIZE){
+            inGame = false;
+        }
+        if(y[0]<0){
+            inGame = false;
+        }
     }
 
     @Override
