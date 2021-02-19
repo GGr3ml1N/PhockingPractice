71a5397ebcc69e833c98ea95abb9d009563db0e5
GGr3ml1N <dinars163@gmail.com>
2021-02-20 01:15:08 +0300

    Синтаксические ошибки + "Ctrl+Alt+L"

--- GameSnake/src/main/java/GameField.java
+++ GameSnake/src/main/java/GameField.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
 
-public class GameField extends JPanel implements ActionListener {
+public class GameField extends JPanel implements ActionListener{
     private final int SIZE = 320;
     private final int DOT_SIZE = 16;
     private final int ALL_DOTS = 400;
@@ -22,89 +22,101 @@ public class GameField extends JPanel implements ActionListener {
     private boolean down = false;
     private boolean inGame = true;
 
-    public GameField() {
+
+    public GameField(){
         setBackground(Color.black);
         loadImages();
+        initGame();
+
     }
 
-    public void initGame() {
+    public void initGame(){
         dots = 3;
         for (int i = 0; i < dots; i++) {
-            x[i] = 48 - i * DOT_SIZE;
+            x[i] = 48 - i*DOT_SIZE;
             y[i] = 48;
         }
-        timer = new Timer(250, this);
+        timer = new Timer(250,this);
         timer.start();
         createApple();
     }
 
-    public void createApple() {
-        appleX = new Random().nextInt(20) * DOT_SIZE;
-        appleY = new Random().nextInt(20) * DOT_SIZE;
+    public void createApple(){
+        appleX = new Random().nextInt(20)*DOT_SIZE;
+        appleY = new Random().nextInt(20)*DOT_SIZE;
     }
 
-    public void loadImages() {
+    public void loadImages(){
         ImageIcon iia = new ImageIcon("GameSnake/res/apple.png");
         apple = iia.getImage();
         ImageIcon iid = new ImageIcon("GameSnake/res/dot.png");
         dot = iid.getImage();
     }
 
-    public void move() {
-        for (int i = dots; i > 0; i--) {
-            x[i] = x[i - 1];
-            y[i] = y[i - 1];
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if(inGame){
+            g.drawImage(apple,appleX,appleY,this);
+            for (int i = 0; i < dots; i++) {
+                g.drawImage(dot,x[i],y[i],this);
+            }
         }
-        if (left) {
+    }
+
+    public void move(){
+        for (int i = dots; i > 0; i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        if(left){
             x[0] -= DOT_SIZE;
         }
-        if (right) {
+        if(right){
             x[0] += DOT_SIZE;
-        }
-        if (up) {
+        } if(up){
             y[0] -= DOT_SIZE;
-        }
-        if (down) {
+        } if(down){
             y[0] += DOT_SIZE;
         }
     }
 
-    private void checkApple() {
-        if (x[0] == appleX && y[0] == appleY) {
+    public void checkApple(){
+        if(x[0] == appleX && y[0] == appleY){
             dots++;
             createApple();
         }
     }
 
-    private void checkCollisions() {
-        for (int i = dots; i > 0; i--) {
-            if (i > 4 && x[0] == x[i] && y[0] == y[i]) {
+    public void checkCollisions(){
+        for (int i = dots; i >0 ; i--) {
+            if(i>4 && x[0] == x[i] && y[0] == y[i]){
                 inGame = false;
             }
         }
 
-        if (x[0] > SIZE) {
+        if(x[0]>SIZE){
             inGame = false;
         }
-        if (x[0] < 0) {
+        if(x[0]<0){
             inGame = false;
         }
-        if (y[0] > SIZE) {
+        if(y[0]>SIZE){
             inGame = false;
         }
-        if (y[0] < 0) {
+        if(y[0]<0){
             inGame = false;
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (inGame) {
-            move();
+        if(inGame){
             checkApple();
             checkCollisions();
+            move();
+
         }
         repaint();
     }
 }
-
