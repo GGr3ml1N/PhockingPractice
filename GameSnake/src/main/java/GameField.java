c298101335982ded65b5491d806465021cc34cb6
GGr3ml1N <dinars163@gmail.com>
2021-02-20 00:13:02 +0300

    исправления

--- GameSnake/src/main/java/GameField.java
+++ GameSnake/src/main/java/GameField.java
@@ -22,50 +22,52 @@ public class GameField extends JPanel implements ActionListener {
     private boolean down = false;
     private boolean inGame = true;
 
-    public GameField(){
+
+    public GameField() {
         setBackground(Color.black);
         loadImages();
         initGame();
+
     }
 
-    public void initGame(){
+    public void initGame() {
         dots = 3;
-        for (int i = 0; i < dots; i++){
-            x[i] = 48 - i*DOT_SIZE;
+        for (int i = 0; i < dots; i++) {
+            x[i] = 48 - i * DOT_SIZE;
             y[i] = 48;
         }
-        timer = new Timer(250,this);
+        timer = new Timer(250, this);
         timer.start();
         createApple();
     }
 
-    public void createApple(){
-        appleX = new Random().nextInt(20)*DOT_SIZE;
-        appleY = new Random().nextInt(20)*DOT_SIZE;
+    public void createApple() {
+        appleX = new Random().nextInt(20) * DOT_SIZE;
+        appleY = new Random().nextInt(20) * DOT_SIZE;
     }
 
-    public void loadImages(){
-        ImageIcon iia = new ImageIcon("apple.png");
+    public void loadImages() {
+        ImageIcon iia = new ImageIcon("res/apple.png");
         apple = iia.getImage();
-        ImageIcon iid = new ImageIcon("dot.png");
+        ImageIcon iid = new ImageIcon("res/dot.png");
         dot = iid.getImage();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (inGame){
-            g.drawImage(apple,appleX,appleY,this);
+        if (inGame) {
+            g.drawImage(apple, appleX, appleY, this);
             for (int i = 0; i < dots; i++) {
-                g.drawImage(dot,x[i],y[i],this);
+                g.drawImage(dot, x[i], y[i], this);
             }
         }
     }
 
-    public void move(){
-        for (int i = dots; i > 0; i-- ){
-            x[i] = x[i-1];
-            y[i] = y[i-1];
+    public void move() {
+        for (int i = dots; i > 0; i--) {
+            x[i] = x[i - 1];
+            y[i] = y[i - 1];
         }
         if (left) {
             x[0] -= DOT_SIZE;
@@ -81,10 +83,39 @@ public class GameField extends JPanel implements ActionListener {
         }
     }
 
+    public void checkApple() {
+        if (x[0] == appleX && y[0] == appleY) {
+            dots++;
+            createApple();
+        }
+    }
+
+    public void checkCollisions() {
+        for (int i = dots; i > 0; i--) {
+            if (i > 4 && x[0] == x[i] && y[0] == y[i]) {
+                inGame = false;
+            }
+        }
+
+        if (x[0] > SIZE) {
+            inGame = false;
+        }
+        if (x[0] < 0) {
+            inGame = false;
+        }
+        if (y[0] > SIZE) {
+            inGame = false;
+        }
+        if (y[0] < 0) {
+            inGame = false;
+        }
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(inGame){
+        if (inGame) {
             checkApple();
+            checkCollisions();
             move();
 
         }
--- GameSnake/src/main/java/MainWindow.java
+++ GameSnake/src/main/java/MainWindow.java
@@ -5,8 +5,8 @@ public class MainWindow extends JFrame {
     public MainWindow() {
         setTitle("Змейка");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(320,345);
-        setLocation(400,400);
+        setSize(320, 345);
+        setLocation(400, 400);
         add(new GameField());
         setVisible(true);
     }
