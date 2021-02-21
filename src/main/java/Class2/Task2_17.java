2b15fef7a0c811d9475ae4b27a95d34d229f705d
GGr3ml1N <dinars163@gmail.com>
2021-02-21 23:26:25 +0300

    2.17

---
+++ src/main/java/Class2/Task2_17.java
@@ -0,0 +1,13 @@
+package Class2;
+
+public class Task2_17 {
+    static int numberEven(int[] array) {
+        int count = 0;
+        for (int i : array) {
+            if (array[i] % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/test/java/Class2/Task2_17Test.java
@@ -0,0 +1,17 @@
+package Class2;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class Task2_17Test {
+
+    @Test
+    public void testNumberEven() {
+        int[] array = new int[10];
+        for(int i=0;i<array.length;i++){
+            array[i] = i;
+        }
+        assertEquals(Task2_17.numberEven(array),5);
+    }
+}
