f5c3bdd58f6c4e9f18cd9d99d5351529c7edecf2
GGr3ml1N <dinars163@gmail.com>
2021-02-21 23:28:03 +0300

    2.33

---
+++ src/main/java/Class2/Task2_33.java
@@ -0,0 +1,9 @@
+package Class2;
+
+public class Task2_33 {
+    static void getArraySting(String[] array) {
+        for (String i : array) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/test/java/Class2/Task2_33Test.java
@@ -0,0 +1,12 @@
+package Class2;
+
+import org.testng.annotations.Test;
+
+public class Task2_33Test {
+
+    @Test
+    public void testGetArraySting() {
+        String[] array = new String[]{"hello","world"};
+        Task2_33.getArraySting(array);
+    }
+}
