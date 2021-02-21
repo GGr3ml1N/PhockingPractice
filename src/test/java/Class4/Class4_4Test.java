0556af0d89348760d28e60d81203a87b9c191f69
GGr3ml1N <dinars163@gmail.com>
2021-02-21 22:50:34 +0300

    4.4

---
+++ src/test/java/Class4/Class4_4Test.java
@@ -0,0 +1,18 @@
+package Class4;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Class4_4Test {
+
+    @Test
+    public void getIntOfString() {
+        System.out.println(Class4_4.getIntOfString("10", "5"));
+        Assert.assertThrows(ArithmeticException.class,
+                () -> Class4_4.getIntOfString("10", "0"));
+        Assert.assertThrows(NumberFormatException.class,
+                () -> Class4_4.getIntOfString("r", "1"));
+        Assert.assertThrows(NumberFormatException.class,
+                () -> Class4_4.getIntOfString("r", "a"));
+    }
+}
