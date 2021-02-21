15ba34f17ccaa93bb9d8b3f9e63f0f713c9d0fbe
GGr3ml1N <dinars163@gmail.com>
2021-02-21 23:26:58 +0300

    2.22

---
+++ src/test/java/Class2/Task2_22Test.java
@@ -0,0 +1,16 @@
+package Class2;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class Task2_22Test {
+
+    @Test
+    public void testGetIndexOftenNumber() {
+        int[] array = new int[]{1,2,3,4,5,6,7};
+        assertEquals(Task2_22.getIndexOftenNumber(array,1),0);
+        assertEquals(Task2_22.getIndexOftenNumber(array,3),2);
+        assertEquals(Task2_22.getIndexOftenNumber(array,7),6);
+    }
+}
