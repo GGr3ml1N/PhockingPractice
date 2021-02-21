07871da6d3a23ea4418b5aaa07737997806d493c
GGr3ml1N <dinars163@gmail.com>
2021-02-21 23:26:10 +0300

    2.15

---
+++ src/main/java/Class2/Task2_15.java
@@ -0,0 +1,13 @@
+package Class2;
+
+public class Task2_15 {
+    static boolean setArray(int[] arr, int number) {
+        for (int j : arr) {
+            if (j == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
