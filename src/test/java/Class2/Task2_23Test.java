cc239f54f9d2ada4e1ace1faa13f3adadad620e9
GGr3ml1N <dinars163@gmail.com>
2021-02-21 23:27:07 +0300

    2.23

---
+++ src/test/java/Class2/Task2_23Test.java
@@ -0,0 +1,16 @@
+package Class2;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class Task2_23Test {
+
+    @Test
+    public void testSetArray() {
+        int[] array = new int[]{1,2,3,4,5,6,7,8};
+        Task2_23.setArray(array);
+        assertEquals(array[0],8);
+        assertEquals(array[7],1);
+    }
+}
