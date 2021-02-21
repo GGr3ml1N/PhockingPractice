510973ed01851e6fcbeae1699f81518ddbab14f4
GGr3ml1N <dinars163@gmail.com>
2021-02-21 23:25:52 +0300

    2.13

---
+++ src/test/java/Class2/Task2_13Test.java
@@ -0,0 +1,19 @@
+package Class2;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class Task2_13Test {
+
+    @Test
+    public void testGetArr() {
+        int[] arr = Task2_13.getArr(11);
+        double accuracy = 0.0001;
+        assertEquals(arr[0], 1, accuracy);
+        assertEquals(arr[10], 1, accuracy);
+        assertEquals(arr[1], 2, accuracy);
+        assertEquals(arr[9], 2, accuracy);
+        assertEquals(arr[5], 6, accuracy);
+    }
+}
