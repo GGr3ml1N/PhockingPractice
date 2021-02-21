6299a28ea4c75842d3457ab2d1a5d9a7b87ced45
GGr3ml1N <dinars163@gmail.com>
2021-02-21 23:26:02 +0300

    2.14

---
+++ src/main/java/Class2/Task2_14.java
@@ -0,0 +1,12 @@
+package Class2;
+
+
+public class Task2_14 {
+    static void setArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = -((Integer) array[i]);
+
+        }
+    }
+}
+
