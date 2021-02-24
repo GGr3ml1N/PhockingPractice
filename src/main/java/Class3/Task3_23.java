85b3b73585659bd3ba171d6a19b1082e232b8677
GGr3ml1N <dinars163@gmail.com>
2021-02-24 21:05:02 +0300

    3.23

---
+++ src/main/java/Class3/Task3_23.java
@@ -0,0 +1,9 @@
+package Class3;
+
+import java.nio.charset.Charset;
+
+public class Task3_23 {
+    public static void main(String[] args) {
+        System.out.println(Charset.defaultCharset());
+    }
+}
