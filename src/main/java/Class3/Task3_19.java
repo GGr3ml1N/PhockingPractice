217f4b94f5a5e76262ca23bc2d42ecae07a0739f
GGr3ml1N <dinars163@gmail.com>
2021-02-24 21:04:36 +0300

    3.19

---
+++ src/main/java/Class3/Task3_19.java
@@ -0,0 +1,23 @@
+package Class3;
+
+import java.util.Arrays;
+
+public class Task3_19 {
+    static String[] parseStringOnArray(String str) {
+        String[] arrayStr = str.split(" ");
+        return Arrays.stream(arrayStr)
+                .filter(s -> !s.isEmpty())
+                .map(s -> s.replace(
+                        String.valueOf(s.charAt(0)),
+                        String.valueOf(s.charAt(0)).toUpperCase()
+                ))
+                .toArray(String[]::new);
+
+//        for (int i = 0; i < arrayStr.length; i++) {
+//            if (!arrayStr[i].isEmpty()) {
+//                arrayStr[i] = arrayStr[i].substring(0, 1).toUpperCase() + arrayStr[i].substring(1);
+//            }
+//        }
+//        return arrayStr;
+    }
+}
