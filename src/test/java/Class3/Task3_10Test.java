b8bd0a320ebc51e8f4867f68a830dc5c8da2e939
GGr3ml1N <dinars163@gmail.com>
2021-02-24 21:04:06 +0300

    3.10

---
+++ src/test/java/Class3/Task3_10Test.java
@@ -0,0 +1,15 @@
+package Class3;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class Task3_10Test {
+
+    @Test
+    public void testSet() {
+        String[]array = new String[]{"Understandable","have","a","nice","day"};
+        assertEquals( Task3_10.set(array,"h","e"),1);
+        assertEquals(Task3_10.set(array,"a","e"),0);
+    }
+}
